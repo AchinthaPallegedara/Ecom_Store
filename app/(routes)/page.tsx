568c50b35fb45aede52fb91c8f6f933af1c39708
Achintha Pallegedara <achinthahirudika@gmail.com>
2024-08-27 19:06:34 +0530

    chore: Update homepage banner image and copyright year

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,20 +8,18 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("65db1c54-bcea-41f0-902e-11d21ff8afa5");
+  const billboard = await getBillboard(process.env.HOME_BANNER_ID || "");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
     <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-          &copy; 2023 Store, Inc. All rights reserved. Developed by SeaX
+          &copy; 2024 Store, Inc. All rights reserved. Developed by Claviq
         </p>
       </div>
     </footer>
-  )
+  );
 };
 
 export default Footer;
